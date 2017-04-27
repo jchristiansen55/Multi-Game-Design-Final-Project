a239e82555d437e3639add537669a8b4f4c550a0
jchristiansen55 <jchrist1@mail.sfsu.edu>
2017-04-27 16:56:35 -0700

    Added Game Manager script
    added game manager script and linked spawns to it, as well as connect
    the game to the server for now. Will continue around 12 tonight

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameManager : Photon.MonoBehaviour {
+
+	public GameObject[] redSpawn;
+	public GameObject[] blueSpawn;
+
+	public int state = 0;
+
+	void Connect(){
+		PhotonNetwork.ConnectToBestCloudServer ("v1.0");
+			state = 1;		
+		}
+	
+
+	void Start () 
+	{
+		
+	}
+
+	void Update () 
+	{
+	
+	}
+		void OnGUI(){
+		switch (state) {
+		case 0:	
+			if(GUI.Button(new Rect(10,10,100,30), "Connect")){
+				Connect ();
+			}
+			 break;
+		case 1:
+			GUI.Label (new Rect (10, 10, 100, 30), "Connected");
+			 break;
+		}
+	}
+}
