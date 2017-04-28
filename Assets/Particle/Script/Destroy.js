2af2bb55049b976d5d5d9af0f28e373c51e39269
jchristiansen55 <jchrist1@mail.sfsu.edu>
2017-04-28 15:34:17 -0700

    ADDED AMAZING PARTICLE EFFECTS WOOOOOHHHHH
    so many nice particle effects we can use for everything AHH SO COOOL we
    can even make the enviorment more magicy and lively by setting some on
    loop and just placing them on rocks and stuff and def for champion
    abilities AHH

---
+++ Assets/Particle/Script/Destroy.js
@@ -0,0 +1,8 @@
+
+var destroyTime:float=3;
+
+function Start () {
+Destroy (gameObject, destroyTime);
+}
+function Update () {
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+
 
 public class GameManager : Photon.MonoBehaviour {
 
@@ -9,8 +10,18 @@ public class GameManager : Photon.MonoBehaviour {
 	public int state = 0;
 
 	void Connect(){
-		PhotonNetwork.ConnectToBestCloudServer ("v1.0");
-			state = 1;		
+		PhotonNetwork.ConnectUsingSettings("v1.0");
+		}
+
+	void OnJoinedLobby(){
+		state = 1;
+		}
+
+	void OnPhotonRandomJoinFailed(){
+		PhotonNetwork.CreateRoom (null);
+		}
+	void OnJoinedRoom(){
+		state = 2;
 		}
 	
 
@@ -31,8 +42,25 @@ public class GameManager : Photon.MonoBehaviour {
 			}
 			 break;
 		case 1:
-			GUI.Label (new Rect (10, 10, 100, 30), "Connected");
+			GUI.Label (new Rect (10, 40, 100, 30), "Connected");
+			if (GUI.Button (new Rect (10, 10, 100, 30), "Que")) {
+				PhotonNetwork.JoinRandomRoom();
+			}
 			 break;
+		case 2: 
+			//Champ Select
+			GUI.Label (new Rect (10, 40, 200, 30), "Select Challenger");
+			if (GUI.Button (new Rect (70, 10, 100, 30), "MagePlaceHolder")) {
+				Spawn (0, "MagePlaceHolder");
+			}
+			break;
+				case 3: 
+				// IN GAME
+			break; 
 		}
 	}
+		void Spawn(int team, string challenger){
+			state = 3; 
+			Debug.Log("Team ... " + team + " Playing as " + challenger);
+		}
 }
