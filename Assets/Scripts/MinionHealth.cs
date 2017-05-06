2297ba1a14ff0c209098344fa3509f9889f6902d
raghavparti <raghav.parti@gmail.com>
2017-05-06 12:11:19 -0700

    added minion hps
    tried adding yasuo hp but it bugged out and wasnt attaching to prefab so
    i undid that part

--- Assets/Scripts/MinionHealth.cs
+++ Assets/Scripts/MinionHealth.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class MinionHealth : MonoBehaviour {
 
+    public float TotalHp;
+    public float CurrentHp;
+
 	// Use this for initialization
 	void Start () {
-		
+        CurrentHp = TotalHp;
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		if (Input.GetMouseButtonDown(0))
+        {
+            TakeDamage();
+        }
 	}
+    void TakeDamage()
+    {
+        CurrentHp -= 5;
+        transform.localScale = new Vector3((CurrentHp / TotalHp), 1, 1);
+
+    }
 }
