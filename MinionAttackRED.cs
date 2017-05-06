89cb801672b9969744b4fba5292947a530c52fd9
phins <phiny91@gmail.com>
2017-05-06 14:29:01 -0700

    Add files via upload

---
+++ MinionAttackRED.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinionAttackRED : MonoBehaviour
+{
+
+    public Transform target;
+
+
+    public float speed = 10f;
+
+    private Transform wayp;
+    private int wavepointIndex = 2;
+    public float range = 15f;
+
+    public bool walk;
+
+    //public GameObject bulletPrefab;
+    //public float fireRate = 1f;
+    //private float fireCountdown = 0f;
+
+    public string enemyTag = "Blue";
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+        wayp = Waypoints.points[2];
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+            walk = false;
+            //targetEnemy = nearestEnemy.GetComponent<Enemy>();
+        }
+        else
+        {
+            target = null;
+            walk = true;
+        }
+    }
+
+    void Update()
+    {
+        // if (GetComponent.
+        if (walk)
+        {
+            Vector3 dir = wayp.position - transform.position;
+            transform.Translate(dir.normalized * speed * Time.deltaTime);
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
