ea26e0a264494078ed0004fa29a6cd58379912df
raghavparti <raghav.parti@gmail.com>
2017-05-13 15:03:58 -0700

    added camera script, working on panning

---
+++ Assets/CameraSript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSript : MonoBehaviour
+{
+
+    public float panSpeed = 30f;
+    public float panBorderThickness = 10f;
+
+    public float scrollSpeed = 5f;
+    public float minY = 10f;
+    public float maxY = 80f;
+
+    void Update()
+    {
+        //Commented out camera movement by mouse.
+        if (Input.mousePosition.x >= Screen.height - panBorderThickness)
+        {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.mousePosition.x <= panBorderThickness)
+        {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.mousePosition.y >= Screen.width - panBorderThickness)
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.mousePosition.y <= panBorderThickness)
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+       
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        Vector3 pos = transform.position;
+        pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        transform.position = pos;
+       
+    }
+}
