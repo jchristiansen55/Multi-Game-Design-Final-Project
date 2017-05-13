f38030bea37e16c901d6a4bcf41e984c68e54b3a
jchristiansen55 <jchrist1@mail.sfsu.edu>
2017-05-13 12:20:02 -0700

    turrets now shoot
    still gota set firepoint for each one but thats for polishing

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,14 +6,14 @@ public class Bullet : MonoBehaviour {
 
 	public float speed = 70f;
 
-	public int damage = 50;
-
 	public GameObject impactEffect;
 
 	public void Seek (Transform _target)
 	{
 		target = _target;
 	}
+
+
 	void Update () {
 
 		if (target == null)
@@ -25,8 +25,20 @@ public class Bullet : MonoBehaviour {
 		Vector3 dir = target.position - transform.position;
 		float distanceThisFrame = speed * Time.deltaTime;
 
+		if (dir.magnitude <= distanceThisFrame)
+		{
+			HitTarget();
+			return;
+		}
+
 		transform.Translate(dir.normalized * distanceThisFrame, Space.World);
 		transform.LookAt(target);
 
+	}
+
+	void HitTarget ()
+	{
+		GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+		Destroy(effectIns,3f);
 	}
 }
