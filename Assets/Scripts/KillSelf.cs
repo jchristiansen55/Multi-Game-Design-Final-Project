f84f2980e2ddd34da8c51db3388f852f3e914cfc
phins <phiny91@gmail.com>
2017-05-19 07:39:44 -0700

    after HOURS, mage - E does aoe dmg!!

--- Assets/Scripts/KillSelf.cs
+++ Assets/Scripts/KillSelf.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class KillSelf : MonoBehaviour {
 
 	public float timer;
+    private Minions tar;
+
+    public float AbilityDMG = 0f;
 
 	void Start () {
 		
@@ -14,5 +17,18 @@ public class KillSelf : MonoBehaviour {
 		if (timer < 0) {
 			Destroy (this.gameObject);
 		}
+
+        Collider[] col = Physics.OverlapSphere(transform.position, 6.0f);
+        foreach (Collider hit in col)
+        {
+            tar = hit.GetComponent<Minions>();
+            if (hit.tag == "Blue")
+            {
+                if (tar != null)
+                {
+                    tar.TakeDamage(AbilityDMG);
+                }
+            }
+        }
 	}
 }
