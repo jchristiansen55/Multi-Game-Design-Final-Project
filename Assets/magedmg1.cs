b973dcdbe4c17e93c54b547ab759e5aa7889a7c1
jchristiansen55 <jchrist1@mail.sfsu.edu>
2017-05-25 17:09:28 -0700

    added Q damage ( hitbox still a bit buggy )

--- Assets/magedmg1.cs
+++ Assets/magedmg1.cs
@@ -4,18 +4,34 @@ using UnityEngine;
 
 public class magedmg1 : MonoBehaviour {
 
-    private Minions tar;
+	private Minions tar;
 
-    public float AbilityDMG = 0f;
-    public float AbilityRange = 0f;
+	public float AbilityDMG = 0f;
+	public Collider boxCollider;  
+	// Use this for initialization
+	void Start () {
 
-    // Use this for initialization
-    void Start () {
-		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
-    }
+		Collider[] col = Physics.OverlapBox (boxCollider.bounds.center, boxCollider.bounds.size);
+		foreach (Collider hit in col)
+		{
+			tar = hit.GetComponent<Minions>();
+			if (hit.tag == "Blue")
+			{
+				if (tar != null)
+				{
+					tar.TakeDamage(AbilityDMG);
+				}
+			}
+		}
+	}
+	void OnDrawGizmosSelected()
+	{
+	//	Gizmos.color = Color.red;
+		//Gizmos.DrawWireSphere(transform.position, range);
+	}
 }
